87c6e0292550f3099bd1acde70025651c04486e1
Bowei Chi <bowei.chi@gmail.com>
2019-12-03 20:38:01 -0800

    Add retry for smtp server disconnected error

--- emailSender.py
+++ emailSender.py
@@ -1,17 +1,18 @@
-from smtplib import SMTP
-from email.mime.text import MIMEText
-import json
-import socket
 import sys
+import socket
+from time import sleep
+from smtplib import SMTP, SMTPServerDisconnected
+from email.mime.text import MIMEText
 
 
 class EmailSender:
     def __init__(self, host, port, from_addr, logger):
-
         self.host = host
         self.port = port
         self.from_addr = from_addr
         self.logger = logger
+        self.server = None
+        self.max_sleep_time = 32   # max sleep time in sec to allow retry
 
     def connect(self):
         try:
@@ -19,21 +20,39 @@ class EmailSender:
         except socket.timeout:
             self.logger.error("Error: host connection timed out.")
         except socket.gaierror:
-            self.logger.error("Error: unknown host name: %s" % host)
+            self.logger.error("Error: unknown host name: %s" % self.host)
         except socket.error:
-            self.logger.error("Error: Connection refused for %s" % host)
+            self.logger.error("Error: Connection refused for %s" % self.host)
 
     def send(self, to_addr, subj, body):
         msg = MIMEText(body)
         msg['Subject'] = subj
         msg['From'] = self.from_addr
         msg['To'] = ','.join(to_addr)
+        sleep_time = 1
 
-        try:
-            self.server.sendmail(self.from_addr, to_addr, msg.as_string())
-            self.logger.info("Emailed to: %s" % to_addr[0])
-        except:
-            self.logger.error("to_addr "+ ','.join(to_addr) + " has unexpected error: " + str(sys.exc_info()[0]))
+        while True:
+            # Allow retry of server disconnect
+            try:
+                self.server.sendmail(self.from_addr, to_addr, msg.as_string())
+                self.logger.info("Emailed to: %s" % to_addr[0])
+                break
+            except SMTPServerDisconnected:
+                if sleep_time <= self.max_sleep_time:
+                    sleep(sleep_time)
+                    sleep_time *= 2
+                    self.logger.warning("SMTP disconnected, will retry...")
+                    self.connect()
+                else:
+                    self.logger.error("to_addr " + ','.join(to_addr) +
+                                      " max retry on disconect reached")
+                    break
+            except:
+                # Some other exception
+                self.logger.error("to_addr " + ','.join(to_addr) +
+                                  " has unexpected error: " +
+                                  str(sys.exc_info()[0]))
+                break
 
     def quit(self):
         self.server.quit()
